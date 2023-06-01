86cc7d28784d16ec2e5e544f2c2d5988caeaa8ed
Willian Dutra <contatowilliandutra@gmail.com>
2023-06-01 16:35:50 -0300

    feat: Implementando algoritimo iterativo para descobrir palindromo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,17 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Retorna se a palavra passada é um palíndromo"""
+    if not word:
+        return False
+
+    word_array = list(word)
+    right = len(word) - 1
+    for letter in word:
+        if letter == word_array[right]:
+            if len(word) // 2 >= right:
+                return True
+            right -= 1
+        else:
+            return False
+
+
+print(is_palindrome_iterative("civic"))
