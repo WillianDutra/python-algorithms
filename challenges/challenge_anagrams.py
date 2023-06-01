f2f68d5f5535c74049e14d2bda32e45f8a1018dc
Willian Dutra <contatowilliandutra@gmail.com>
2023-06-01 15:10:49 -0300

    feat: Implementando algoritimo para verificar anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,43 @@
+def sort(letters, start, end):
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        sort(letters, start, mid)
+        sort(letters, mid, end)
+        merge(letters, start, mid, end)
+
+
+def merge(letters, start, mid, end):
+    left = letters[start:mid]
+    right = letters[mid:end]
+    left_index, right_index = 0, 0
+
+    for i in range(start, end):
+        if left_index >= len(left):
+            letters[i] = right[right_index]
+            right_index += 1
+        elif right_index >= len(right):
+            letters[i] = left[left_index]
+            left_index += 1
+        elif left[left_index] < right[right_index]:
+            letters[i] = left[left_index]
+            left_index += 1
+        else:
+            letters[i] = right[right_index]
+            right_index += 1
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Retorna se uma string é um anagrama da outra"""
+    if first_string == "" and second_string == "":
+        return ("", "", False)
+
+    first_arr = list(first_string.lower())
+    second_arr = list(second_string.lower())
+
+    sort(first_arr, 0, len(first_arr))
+    sort(second_arr, 0, len(second_arr))
+
+    first = "".join(first_arr)
+    second = "".join(second_arr)
+
+    return (first, second, first == second)
--- challenges/challenge_encrypt_message.py
+++ challenges/challenge_encrypt_message.py
@@ -21,19 +21,12 @@ def encrypt_message(message: str, key: int):
 
 
 def test_encrypt_message():
-    'Para um valor de "key" incorreto, retorna um erro'
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message("message", "2")
 
-    'Para um valor de "message" incorreto, retorna um erro'
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(123123, 2)
 
-    'Para um valor de "key" negativo, retorna a string invertina'
     assert encrypt_message("message", -1) == "egassem"
-
-    'Para um valor impar de "key", retorna a string dividida'
     assert encrypt_message("message", 3) == "sem_egas"
-
-    'Para um valor par de "key", retorna a string dividida'
     assert encrypt_message("message", 2) == "egass_em"
