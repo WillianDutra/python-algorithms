aef10426db5f5a6756c42d02bb74dec0939c6849
Willian Dutra <contatowilliandutra@gmail.com>
2023-06-01 12:09:40 -0300

    feat: Implementando algoritimo para busca de estudantes estudando

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Retorna o número de estudantes presentes no horário escolhido"""
+    if not target_time:
+        return None
+
+    count = 0
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+        if period[0] <= target_time <= period[1]:
+            count += 1
+
+    return count
