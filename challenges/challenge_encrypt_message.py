2799c66179063aeb38714628f6d9d4ca949f363d
Willian Dutra <contatowilliandutra@gmail.com>
2023-06-01 12:49:10 -0300

    test: Implementando testes para o algoritimo que encripta mensagens

--- challenges/challenge_encrypt_message.py
+++ challenges/challenge_encrypt_message.py
@@ -1,5 +1,7 @@
-def encrypt_message(message: str, key: int):
+import pytest
 
+
+def encrypt_message(message: str, key: int):
     if not isinstance(key, int):
         raise TypeError("tipo inválido para key")
 
@@ -16,3 +18,22 @@ def encrypt_message(message: str, key: int):
         part_two, part_one = part_one, part_two
 
     return "".join(part_one) + "_" + "".join(part_two)
+
+
+def test_encrypt_message():
+    'Para um valor de "key" incorreto, retorna um erro'
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message", "2")
+
+    'Para um valor de "message" incorreto, retorna um erro'
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123123, 2)
+
+    'Para um valor de "key" negativo, retorna a string invertina'
+    assert encrypt_message("message", -1) == "egassem"
+
+    'Para um valor impar de "key", retorna a string dividida'
+    assert encrypt_message("message", 3) == "sem_egas"
+
+    'Para um valor par de "key", retorna a string dividida'
+    assert encrypt_message("message", 2) == "egass_em"
